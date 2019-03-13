2e8f5fc41a3e6d6ab47e27c5ee6991756936c0e9
asuvorov <alex.suv89@gmail.com>
2019-03-13 21:57:59 +0300

    remove custom kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import TitleBar from './elements/TitleBar';
-import { ThemeProvider } from './styles/ThemeProvider';
 
 class App extends Component {
   render() {
     return (
-      <ThemeProvider>
-        <TitleBar/>
-        <div>app</div>
-      </ThemeProvider>
+      App
     );
   }
 }
